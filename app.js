158bedba287493cfa252f8ac1732b6294201cdd9
Lisa Carbonell <morekaleplease@gmail.com>
2019-03-27 18:05:09 -0700

    projects object literal array

---
+++ app.js
@@ -0,0 +1,16 @@
+/*  from using Pug in your express app video 4 of 2nd pug
+
+// by default express will look in folder called views in the root of your project
+
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug'); //tell express (set the view engine) to use pug
+
+app.get('/', (req, res) => {
+    res.send('');
+});
+
+*/ 
